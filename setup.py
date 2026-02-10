bd673fdd1572e35e6596f4b85ca0c9d88d89c592
Jose Caudevilla <jose.caudevilla@trioteca.com>
2026-02-10 09:24:08 +0100

    fix pyannote.audio

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url="https://github.com/m-bain/whisperx",
     license="MIT",
     packages=find_packages(exclude=["tests*"]),
-    install_requires=parse_requirements("requirements.txt") + ["pyannote.audio==3.2.1"],
+    install_requires=parse_requirements("requirements.txt") + ["pyannote.audio==3.1.1"],
     entry_points={
         "console_scripts": ["whisperx=whisperx.transcribe:cli"],
     },
